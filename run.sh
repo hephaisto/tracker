e9192b03aed2b6341bf699c47042de7d5cbc888c
hephaisto <1156478+hephaisto@users.noreply.github.com>
2026-04-27 02:28:22 +0200

    fix run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ set -e
 set -x
 scriptdir=$(cd $(dirname $0) && pwd)
 project_name=$(basename "$scriptdir")
-podman run -it --publish [::]:8085:8080 ${project_name}:local $*
+podman run -it --publish 127.0.0.1:8085:8080 ${project_name}:local 0.0.0.0
